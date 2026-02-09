570554012cb8587135c26d41295c30c4d9e6fd37
hmalykhan <hmalykhan@gmail.com>
2026-02-09 16:59:20 +0500

    delay is increased to 3 days.

--- pathzi/settings.py
+++ pathzi/settings.py
@@ -255,8 +255,8 @@ LOGGING = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=2),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=3),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
 }
