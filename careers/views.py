c270d1c621189139261655dc15ed53745e88a485
hmalykhan <hmalykhan@gmail.com>
2026-02-20 16:19:52 +0500

    error resolved.

--- careers/views.py
+++ careers/views.py
@@ -157,7 +157,7 @@ class CareersView(viewsets.ModelViewSet):
         qs = self._filtered_base_queryset().order_by("id")
 
         # list must show only 5 for free users
-        if getattr(self, "action", None) -m ""== "list" and not self._is_subscribed():
+        if getattr(self, "action", None) == "list" and not self._is_subscribed():
             qs = qs[:FREE_CAREER_LIMIT]
 
         return qs
